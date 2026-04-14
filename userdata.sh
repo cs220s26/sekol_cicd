dfe507cbd81392719095428ee303ff045c761dba
Ben Coleman <colemanb@moravian.edu>
2026-04-14 06:34:17 -0400

    Update to cs220s26 instead of s25

--- userdata.sh
+++ userdata.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 yum install git -y
-git clone https://github.com/cs220s25/<repo_name> /<repo_name>
+git clone https://github.com/cs220s26/<repo_name> /<repo_name>
 cd /<repo_name>
 python3 -m venv .venv
 .venv/bin/pip install -r requirements.txt 
